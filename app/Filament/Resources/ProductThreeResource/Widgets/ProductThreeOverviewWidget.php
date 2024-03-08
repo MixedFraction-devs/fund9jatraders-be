2c511bded1395bf1cbcbf0051c7dbcf785bc5d22
Oluwaseun Raphael Afolayan <theafolayan@gmail.com>
2024-03-08 21:30:55 +0100

    fixed product one issues.

--- app/Filament/Resources/ProductThreeResource/Widgets/ProductThreeOverviewWidget.php
+++ app/Filament/Resources/ProductThreeResource/Widgets/ProductThreeOverviewWidget.php
@@ -19,10 +19,10 @@ class ProductThreeOverviewWidget extends BaseWidget
         return [
             Card::make('Remaining Stock', ProductThree::where('status', 'inactive')->count())->description("All accounts remaining"),
             Card::make('Demo Stock', ProductThree::where('status', 'inactive')->where('mode', 'demo')->count())->description("All Demo accounts remaining"),
-            Card::make('Product 2 Orders', ProductThree::whereNot('status', 'inactive')->count())->description('All active accounts that have been purchased'),
-            Card::make('Product 2 Breached', ProductThree::where('status', 'breached')->count())->description('All accounts that have been breached'),
-            Card::make('Product 2 Passed', ProductThree::where('status', 'passed')->count())->description('All accounts that have been passed'),
-            Card::make('Product 2 Real Promoted', ProductThree::whereNotNull('is_assigned')->where('mode', 'real')->count())->description('Real accounts that have been promoted'),
+            Card::make('Product 3 Orders', ProductThree::whereNot('status', 'inactive')->count())->description('All active accounts that have been purchased'),
+            Card::make('Product 3 Breached', ProductThree::where('status', 'breached')->count())->description('All accounts that have been breached'),
+            Card::make('Product 3 Passed', ProductThree::where('status', 'passed')->count())->description('All accounts that have been passed'),
+            Card::make('Product 3 Real Promoted', ProductThree::whereNotNull('is_assigned')->where('mode', 'real')->count())->description('Real accounts that have been promoted'),
         ];
     }
 }
